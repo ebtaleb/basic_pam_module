9bb84e84dc2f58b7866d8c8b31281f2a14913ef2
jmbto <kagekyio@gmail.com>
2014-11-12 13:25:21 +0800

    deny access if phone number empty

--- pam_2step_auth.c
+++ pam_2step_auth.c
@@ -57,7 +57,7 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
         return result;
 
     if (phone_number == NULL)
-        result = PAM_AUTHTOK_ERR;
+        return PAM_AUTHTOK_ERR;
 
     // send PIN via SMS
     char cmd[50] = {0};
