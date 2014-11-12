bc2651a7bc8f263af40f2f4224a5f45e8b7b90f8
jmbto <kagekyio@gmail.com>
2014-11-12 13:37:59 +0800

    salts PINs, refactoring

--- pam_2step_auth.c
+++ pam_2step_auth.c
@@ -7,6 +7,10 @@
 #include <openssl/sha.h>
 #include <sys/time.h>
 
+#define SALT_LENGTH 16
+#define MAX_PIN_LENGTH 6
+#define PIN_EXPIRATION_TIME 60  // in seconds
+
 void seedrand() {
     FILE *f = fopen("/dev/urandom", "r");
     unsigned seed;
@@ -32,11 +36,15 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     char *pin_read = NULL;
     const char *password;
     char *phone_number = NULL;
-    char rand_pin[5]= {0};
+    char rand_pin[MAX_PIN_LENGTH]= {0};
+    char salt[SALT_LENGTH] = {0};
+    char rand_pin_and_salt[SALT_LENGTH+MAX_PIN_LENGTH+1] = {0};
+    char input[SALT_LENGTH+MAX_PIN_LENGTH+1] = {0};
 
-	SHA512_CTX c;
-	unsigned char md1[SHA512_DIGEST_LENGTH];
-	unsigned char md2[SHA512_DIGEST_LENGTH];
+    SHA512_CTX c;
+
+    unsigned char random_salted_hash[SHA512_DIGEST_LENGTH];
+    unsigned char input_salted_hash[SHA512_DIGEST_LENGTH];
 
     struct timeval start, end;
     double time_elapsed;
@@ -48,9 +56,17 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
 
     // pick a OTP PIN between 1000 and 999999
     int num = (999999 - 1000 +1)*(double)rand()/RAND_MAX + 1000;
+
+    // generate a random salt
+    int i;
+    for (i = 0; i < SALT_LENGTH; ++i){
+        salt[i] = '0' + rand() % 72; // starting on '0', ending on '}'
+    }
+
     sprintf(rand_pin, "%d", num);
-    SHA512_Update(&c,rand_pin,(unsigned long)4);
-    SHA512_Final(&(md1[0]),&c);
+    sprintf(rand_pin_and_salt, "%d%s", num, salt);
+    SHA512_Update(&c,rand_pin_and_salt,(unsigned long)strlen(rand_pin_and_salt));
+    SHA512_Final(&(random_salted_hash[0]),&c);
 
     result = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &phone_number, "Phone number? (65xxxxxxxx): ");
     if (result != PAM_SUCCESS)
@@ -73,18 +89,19 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     gettimeofday(&end, NULL);
     time_elapsed = (double) (end.tv_sec - start.tv_sec)
                    + (double) (end.tv_usec - start.tv_usec) / 1000000;
-    if (time_elapsed > 60) {
+    if (time_elapsed > PIN_EXPIRATION_TIME) {
         return PAM_AUTHTOK_ERR;
     }
 
+    sprintf(input, "%s%s", pin_read, salt);
 	SHA512_Init(&c);
-    SHA512_Update(&c,pin_read,(unsigned long)4);
-    SHA512_Final(&(md2[0]),&c);
+    SHA512_Update(&c,input,(unsigned long) (strlen(input)));
+    SHA512_Final(&(input_salted_hash[0]),&c);
 
     if (pin_read == NULL || strlen(pin_read) < 4 || strlen(pin_read) > 6) {
         result = PAM_AUTHTOK_ERR;
     } else {
-        if (cmp_hash(md1, md2))
+        if (cmp_hash(random_salted_hash, input_salted_hash))
             result = PAM_SUCCESS;
         else
             result = PAM_AUTHTOK_ERR;
